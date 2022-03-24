27b03bcd8359373126cc57354129ce8a0169ff37
Oleg Chepelev <yorkblansh@gmail.com>
2022-03-24 19:19:17 +0300

    ignoring some files

--- build/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TYPES = void 0;
-exports.TYPES = {
-    // IOutput: Symbol.for('IOutput'),
-    Dog: Symbol.for('Dog'),
-};
